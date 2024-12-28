07e19e8469cb818a383018f9e505647639f29844
ronaldt80 <53364332+ronaldt80@users.noreply.github.com>
2024-12-28 14:07:40 +0100

    Update __init__.py

--- custom_components/philips_airpurifier_http/__init__.py
+++ custom_components/philips_airpurifier_http/__init__.py
@@ -1,1 +1,8 @@
-""" philips_airpurifier_http platform setup """
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "philips_airpurifier_http"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Setup logic for the integration
+    return True
