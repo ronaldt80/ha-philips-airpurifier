db90cf858fab4d23a9175b8256ab44cfc9609478
ronaldt80 <53364332+ronaldt80@users.noreply.github.com>
2024-12-28 16:09:22 +0100

    Update config_flow.py

--- custom_components/philips_airpurifier_http/config_flow.py
+++ custom_components/philips_airpurifier_http/config_flow.py
@@ -1,6 +1,7 @@
 from homeassistant import config_entries
 import voluptuous as vol
 from .const import DOMAIN
+from pyairctrl.http_client import HTTPAirClient
 
 class PhilipsAirPurifierConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -9,12 +10,18 @@ class PhilipsAirPurifierConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             host = user_input["host"]
             client = await self.hass.async_add_executor_job(lambda: HTTPAirClient(host, False))
-            wifi = client.get_wifi  # Method to retrieve MAC address
-            if PHILIPS_MAC_ADDRESS in wifi:
-                mac_address = wifi[PHILIPS_MAC_ADDRESS]
+            wifi_info = await self.hass.async_add_executor_job(client.get_wifi)  # Method to retrieve MAC address
+            mac_address = wifi_info.get("macaddress")
+            
+            if not mac_address:
+                return self.async_show_form(
+                    step_id="user",
+                    errors={"base": "cannot_retrieve_mac"}
+                )
+
             await self.async_set_unique_id(mac_address)
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title="Philips AirPurifier", data=user_input)
+            return self.async_create_entry(title="Philips AirPurifier (HTTP)", data=user_input)
 
         return self.async_show_form(
             step_id="user",
