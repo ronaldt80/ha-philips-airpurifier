ad98a027b0d899627856f0c2e887dcb59a2aa03a
ronaldt80 <53364332+ronaldt80@users.noreply.github.com>
2024-12-28 14:08:36 +0100

    Update config_flow.py

--- custom_components/philips_airpurifier_http/config_flow.py
+++ custom_components/philips_airpurifier_http/config_flow.py
@@ -7,11 +7,17 @@ class PhilipsAirPurifierConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Philips AirPurifier HTTP", data=user_input)
+            host = user_input["host"]
+            client = await self.hass.async_add_executor_job(lambda: HTTPAirClient(host, False))
+            mac_address = client.get_mac_address()  # Method to retrieve MAC address
+            await self.async_set_unique_id(mac_address)
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title="Philips AirPurifier", data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("host"): str,
+                vol.Required("api_key"): str,
             })
         )
